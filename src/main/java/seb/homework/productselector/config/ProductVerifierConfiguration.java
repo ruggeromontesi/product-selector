3b5e87a1f8b7e8c64ae64f108d5227d725bc3d3e
ruggeromontesi <ruggero.montesi@uniquex.com>
2023-08-23 14:00:04 +0300

    Query products from H2 db

--- src/main/java/seb/homework/productselector/config/ProductVerifierConfiguration.java
+++ src/main/java/seb/homework/productselector/config/ProductVerifierConfiguration.java
@@ -3,7 +3,6 @@ package seb.homework.productselector.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import seb.homework.productselector.core.product.Product;
 import seb.homework.productselector.core.product.verifier.ProductFinder;
 import seb.homework.productselector.db.product.FindProductJdbcAdapter;
 
