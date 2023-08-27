e616571afcc68a8280ecd3e59f309b0899f9e96a
ruggeromontesi <ruggero.montesi@uniquex.com>
2023-08-27 09:41:25 +0300

    create ut for CreateProductController

---
+++ adapters-rest/src/test/java/seb/homework/productselector/product/create/CreateProductControllerTest.java
@@ -0,0 +1,12 @@
+package seb.homework.productselector.product.create;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateProductControllerTest {
+
+   @Test
+   void createProduct() {
+   }
+}
